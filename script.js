7f033a636a9e92118a21b84db3bf3cc09a2d0a3e
Karan <karanchandekar431@gmail.com>
2023-01-10 09:16:12 +0530

    targetting the elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// targeting the parent element
+const blogContainer = document.querySelector(".blog__container");
+const blogModal = document.querySelector(".blog__modal__body");
+// global
+let globalStore = [];
