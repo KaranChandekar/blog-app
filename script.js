7946702b76870678e93ac161279602db596c0dc8
Karan <karanchandekar431@gmail.com>
2023-01-10 09:16:43 +0530

    added function to create a new card

--- script.js
+++ script.js
@@ -3,3 +3,32 @@ const blogContainer = document.querySelector(".blog__container");
 const blogModal = document.querySelector(".blog__modal__body");
 // global
 let globalStore = [];
+
+// -----------------------------------------------------
+// a function for creating a new card
+const newCard = ({
+  id,
+  imageUrl,
+  blogTitle,
+  blogType,
+  blogDescription,
+}) => `<div class="col-lg-4 col-md-6" id=${id}>
+<div class="card m-2">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" class="btn btn-outline-success" id="${id}" onclick="editCard.apply(this, arguments)"><i class="fas fa-pencil-alt" id="${id}" onclick="editCard.apply(this, arguments)"></i></button>
+    <button type="button" class="btn btn-outline-danger" id="${id}" onclick="deleteCard.apply(this, arguments)"><i class="fas fa-trash-alt" id="${id}" onclick="deleteCard.apply(this, arguments)"></i></button>
+  </div>
+  <img
+    src=${imageUrl}
+    class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${blogTitle}</h5>
+    <p class="card-text">${blogDescription}</p>
+    <span class="badge bg-primary">${blogType}</span>
+  </div>
+  <div class="card-footer text-muted">
+    <button type="button" id="${id}" class="btn btn-outline-primary float-end" data-bs-toggle="modal"
+    data-bs-target="#showblog" onclick="openBlog.apply(this, arguments)">Open Blog</button>
+  </div>
+</div>
+</div>`;
